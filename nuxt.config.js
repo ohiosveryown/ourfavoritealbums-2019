32cd627883f6ca61e68aca78afddfdcf75839464
Matthew Pence <matt@ohiosveryown.co>
2020-01-11 00:11:26 -0500

    Initial commit

---
+++ nuxt.config.js
@@ -0,0 +1,33 @@
+
+export default {
+  mode: 'universal',
+  // headers
+  head: {
+    title: process.env.npm_package_name || '',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  // loader
+  loading: false,
+  // global css
+  css: [
+    '~/assets/reset.scss',
+    '~/assets/type.scss',
+    '~/assets/util.scss'
+  ],
+  // nuxt js modules
+  modules: [
+    '@nuxtjs/pwa',
+  ],
+  // build config
+  build: {
+    extend (config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<!-- layout -->
+<template>
+  <div class="container">
+    <h1 class="debug body">Our Favorite Albums</h1>
+  </div>
+</template>
+
+
+<!-- style -->
+<style lang='scss' scoped>
+  @import '~/assets/grid.scss';
+
+  h1 {
+    color: red;
+    @include breakpoint(mdl) { color: purple; }
+  }
+</style>
+
+
+<!-- logic -->
+<script>
+  export default {
+    components: {
+
+    }
+  }
+</script>
