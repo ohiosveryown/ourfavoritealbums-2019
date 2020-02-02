aaa477646d109c136b528859c428bf2a8c21ff46
Matthew Pence <matt@ohiosveryown.co>
2020-02-01 22:27:21 -0500

    Disable gsap scroll on <700

--- logic/forApp.js
+++ logic/forApp.js
@@ -1,72 +1,79 @@
 const scrolling = () => {
-  var html = document.documentElement
-  var body = document.body
 
-  var scroller = {
-    target: document.querySelector(".scroll-container"),
-    ease: 0.05, // <= scroll speed
-    endY: 0,
-    y: 0,
-    resizeRequest: 1,
-    scrollRequest: 0,
-  }
+  const mq = window.matchMedia( '(min-width: 700px)' )
+  const html = document.documentElement
+  const body = document.body
+  const scollContainer = document.querySelector(".scroll-container")
 
-  var requestId = null
-
-  TweenLite.set(scroller.target, {
-    rotation: 0.01,
-    force3D: true
-  })
-
-  window.addEventListener("load", onLoad)
-
-  function onLoad() {
-    updateScroller()
-    window.focus()
-    window.addEventListener("resize", onResize)
-    document.addEventListener("scroll", onScroll)
-  }
-
-  function updateScroller() {
-
-    var resized = scroller.resizeRequest > 0
-
-    if (resized) {
-      var height = scroller.target.clientHeight
-      body.style.height = height + "px"
-      scroller.resizeRequest = 0
+  // for ≥ md
+  if (mq.matches) {
+    const scroller = {
+      target: scollContainer,
+      ease: 0.05, // <= scroll speed
+      endY: 0,
+      y: 0,
+      resizeRequest: 1,
+      scrollRequest: 0,
     }
 
-    var scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0
-
-    scroller.endY = scrollY
-    scroller.y += (scrollY - scroller.y) * scroller.ease
-
-    if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
-      scroller.y = scrollY
-      scroller.scrollRequest = 0
-    }
+    let requestId = null
 
     TweenLite.set(scroller.target, {
-      y: -scroller.y
-    });
+      rotation: 0.01,
+      force3D: true
+    })
 
-    requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null
-  }
+    window.addEventListener("load", onLoad)
 
-  function onScroll() {
-    scroller.scrollRequest++
-    if (!requestId) {
-      requestId = requestAnimationFrame(updateScroller)
+    function onLoad() {
+      updateScroller()
+      window.focus()
+      window.addEventListener("resize", onResize)
+      document.addEventListener("scroll", onScroll)
+    }
+
+    function updateScroller() {
+
+      let resized = scroller.resizeRequest > 0
+
+      if (resized) {
+        let height = scroller.target.clientHeight
+        body.style.height = height + "px"
+        scroller.resizeRequest = 0
+      }
+
+      let scrollY = window.pageYOffset || html.scrollTop || body.scrollTop || 0
+
+      scroller.endY = scrollY
+      scroller.y += (scrollY - scroller.y) * scroller.ease
+
+      if (Math.abs(scrollY - scroller.y) < 0.05 || resized) {
+        scroller.y = scrollY
+        scroller.scrollRequest = 0
+      }
+
+      TweenLite.set(scroller.target, {
+        y: -scroller.y
+      });
+
+      requestId = scroller.scrollRequest > 0 ? requestAnimationFrame(updateScroller) : null
+    }
+
+    function onScroll() {
+      scroller.scrollRequest++
+      if (!requestId) {
+        requestId = requestAnimationFrame(updateScroller)
+      }
+    }
+
+    function onResize() {
+      scroller.resizeRequest++
+      if (!requestId) {
+        requestId = requestAnimationFrame(updateScroller)
+      }
     }
   }
 
-  function onResize() {
-    scroller.resizeRequest++
-    if (!requestId) {
-      requestId = requestAnimationFrame(updateScroller)
-    }
-  }
 }
 
 
